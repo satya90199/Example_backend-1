366bdfe71ad7130bfaa15a89a830911748dac860
satya90199 <satyanarayan.programmics@gmail.com>
2023-12-30 12:39:23 +0530

    default commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -52,7 +52,7 @@ $app->singleton(
 | This script returns the application instance. The instance is given to
 | the calling script so we can separate the building of the instances
 | from the actual running of the application and sending responses.
-|
+|default commitment
 */
 
 return $app;
